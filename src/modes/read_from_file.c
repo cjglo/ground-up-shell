2170e4dccf5a78c0572b2d37612b6bf1d643bed3
cjglo <chris.j.gallo3@gmail.com>
2022-06-21 00:48:53 -0400

    finished file_to_string and added/cleaned comments

--- src/modes/read_from_file.c
+++ src/modes/read_from_file.c
@@ -18,126 +18,78 @@ int read_from_file(FILE* file) {
         }
 
     }
-
-    // for(int i = 0; buffer[i][0] != '\0'; i++) {
-    //     printf("Should be: %s\n", buffer[i]);
-
-    //     // for(int x = 0; buffer[i][x] != '\0'; x++) {
-    //     //     printf("%d ", buffer[i][x]);
-    //     // }
-
-    //     int begin = 0;
-    //     int end = 0;
-    //     char** args = malloc(0); // holds args
-    //     int last_arg = -1;
-    //     for(int j = 0; true; j++) {
-    //         // TODO: best way to copy first arg?
-            
-    //         // printf("begin: %d, end: %d, ij: %d\n", begin, end, buffer[i][j]);
-    //         // go along string
-    //         // each space or last '\0: 
-    //             // realloc arr for one more spot and copy contents
-    //             // add new arg 
-
-    //         if(buffer[i][j] == ' ') {
-    //             // printf("HERE2\n");
-    //             int word_size = end - begin;
-    //             // realloc args and copy in new word
-    //             args = (char**) realloc(args, sizeof(char*));
-    //             args[++last_arg] = malloc( sizeof(char*)*word_size); // TODO: Need plus 1?
-                
-    //             for(int k = begin; k<word_size; k++) {
-    //                 args[last_arg][k] = buffer[i][k];
-    //             }
-    //             args[last_arg][end] = '\0';
-    //             begin = j+1;// move to begin of next word
-    //         } 
-    //         if(buffer[i][j] == '\0') { // EXIT CONDITION
-    //             // printf("HERE!");
-    //             // int word_size = end - begin - 1;
-    //             // // realloc args and copy in new word
-    //             // args = (char**) realloc(args, sizeof(char*));
-    //             // args[++last_arg] = malloc( sizeof(char*)*word_size+1); // TODO: Need plus 1?
-    //             // memcpy(args[last_arg], &buffer[i][begin], end - 1);
-    //             // args[last_arg][end] = '\0';
-    //             // begin = j+1; // move to begin of next word
-    //             break;
-    //         }
-    //         // memcpy( subbuff, &buff[10], 4 );  
-    //         end++;
-    //     }
-    //     printf("%s\n", args[0]);
-    //     printf("%s\n", args[1]);
-    //     printf("%s\n", args[2]);
-    // }
-    // printf("\n");
     
     return FILE_EXEC_SUCCESS;
 }
 
 char*** file_to_strings(FILE* file) {
 
+    /*
+    This function breaks the file into an array of lines, 
+    which are arrays of words (and words are C-strings).
+
+    Example File:
+        This is an
+        example file
+    
+    Will become:
+        [
+            ["This", "is", "an"],
+            ["example", "file"]
+        ]
+    */
+
     int number_of_lines = 1;
     char ch = fgetc(file);
     int test_i = 0;
+    // first, count lines to malloc outer ptr
     while(ch != EOF) {
         if(ch == '\n') number_of_lines++;
         ch = fgetc(file);
     }
 
-    char*** str_lines = malloc(sizeof(char***)*(number_of_lines+1));
-    str_lines[number_of_lines] = NULL; // to mark end
-    // restart file pointer to now copy file to str
+    char*** lines_of_words = malloc(sizeof(char***)*(number_of_lines+1));
+    lines_of_words[number_of_lines] = NULL; // NULL used to mark end of outer two ptrs
 
-
-    printf("\nHERE!\n");
-
-    rewind(file);
-    ch = fgetc(file);
+    // TODO: Could combine these into one loop and use realloc, not sure if cleaner / more efficient
+    rewind(file); // restart file
     int number_of_words = 0;
     int line = 0;
-    // TODO: Could combine these into one loop and use realloc, not sure if cleaner / more efficient
+    // now count words so can alloc each index in lines with right # of words
     do {
+        ch = fgetc(file);
         if(ch == ' ') {
             number_of_words++;
         } else if(ch == '\n' || ch == EOF) {
             number_of_words++;
-            str_lines[line] = malloc(sizeof(char**)*(number_of_words+1));
-            str_lines[line][number_of_words] = NULL; // to mark end
+            lines_of_words[line] = malloc(sizeof(char**)*(number_of_words+1));
+            lines_of_words[line][number_of_words] = NULL; // NULL used to mark end of outer two ptrs
             number_of_words = 0;
             line++;
         }
-        ch = fgetc(file);
+        
     } while(ch != EOF);
 
-    printf("\nHERE!22\n");
-
-
-    rewind(file);
-    ch = fgetc(file);
+    rewind(file); // restart again
     int word_len = 0;
     line = 0;
     int word_index = 0;
+    // now alloc each word based on length of word
     do {
-        printf("%c\n", ch);
+        ch = fgetc(file);
         if(ch == ' ') {
-            printf("line: %d, word_i: %d, word_len: %d\n", line, word_index, word_len);
-            str_lines[line][word_index] = malloc(sizeof(char*)*(word_len+1));
+            lines_of_words[line][word_index] = malloc(sizeof(char*)*(word_len+1));
             word_index++;
             word_len = 0;
         } else if(ch == '\n' || ch == EOF) {
-            str_lines[line][word_index] = malloc(sizeof(char*)*(word_len+1));
+            lines_of_words[line][word_index] = malloc(sizeof(char*)*(word_len+1));
             line++;
             word_index = 0;
             word_len  = 0;
         } else {
             word_len++;
         }
-        ch = fgetc(file);
     } while(ch != EOF);
-
-    printf("\nHERE!33\n");
-
 
     rewind(file);
     ch = fgetc(file);
@@ -145,23 +97,24 @@ char*** file_to_strings(FILE* file) {
     line = 0;
     word_index = 0;
     int char_index = 0;
+    // finally, we can now move each char into its spot
     while(ch != EOF) {
         if(ch == ' ') {
-            str_lines[line][word_index][char_index] = '\0';
+            lines_of_words[line][word_index][char_index] = '\0';
             char_index = 0;
             word_index++;
         } else if(ch == '\n') {
-            str_lines[line][word_index][char_index] = '\0';
+            lines_of_words[line][word_index][char_index] = '\0';
             line++;
             word_index = 0;
             char_index = 0;
         } else {
-            str_lines[line][word_index][char_index] = ch;
+            lines_of_words[line][word_index][char_index] = ch;
             char_index++;
         }
         ch = fgetc(file);
     }
-    str_lines[line][word_index][char_index] = '\0';
+    lines_of_words[line][word_index][char_index] = '\0';
 
-    return str_lines;
+    return lines_of_words;
 }
